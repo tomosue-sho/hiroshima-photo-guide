f75713b84933354b2ad2bd2bd45cd3bb3aec58cd
tomosue-sho <tomotomosho1006@gmail.com>
2026-06-21 18:22:44 +0900

    use env vars for production

--- config/settings.py
+++ config/settings.py
@@ -130,9 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
