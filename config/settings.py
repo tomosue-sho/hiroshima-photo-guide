255a0239e9faf3e65a150c6d1e5efc571ed1963d
tomosue-sho <tomotomosho1006@gmail.com>
2026-06-22 19:21:51 +0900

    use env vars for production

--- config/settings.py
+++ config/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 AWS_ACCESS_KEY_ID = os.environ.get("R2_ACCESS_KEY_ID")
