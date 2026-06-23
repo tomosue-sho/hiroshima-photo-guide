fb0f5e4dd12b8b57a93b6416dab9b412aba11e2c
tomosue-sho <tomotomosho1006@gmail.com>
2026-06-23 16:14:36 +0900

    improve seo title and meta description

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ import dj_database_url
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-only-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     "localhost",
